b522d3da948510ea3f41b04dc48d91924c2f9cae
SomaTakata <somatakata@gmail.com>
2024-12-25 23:57:59 +0900

    mvn clean install

--- backend/src/test/java/com/devbox/mavenapp/AppTest.java
+++ backend/src/test/java/com/devbox/mavenapp/AppTest.java
@@ -1,20 +1,12 @@
 package com.devbox.mavenapp;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true, "This should always pass");
     }
 }
