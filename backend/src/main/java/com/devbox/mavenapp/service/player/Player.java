d5e8d98a04ff1f95c10cfa0c8a567d4737851de1
Kira <kira.a0121@gmail.com>
2024-12-30 10:43:24 +0900

    player(仮)作成

---
+++ backend/src/main/java/com/devbox/mavenapp/service/player/Player.java
@@ -0,0 +1,31 @@
+package com.devbox.mavenapp.service.player;
+
+public class Player {
+    char playerID;
+    int position;
+    char turnStatus;
+    int score;
+    //List<item> item;
+    int moveSpaces;
+
+    public Player(char playerID,int position,char turnStatus,int score,int moveSpaces){
+        this.playerID=playerID;
+        this.position=position;
+        this.turnStatus=turnStatus;
+        this.score=score;
+        this.moveSpaces=moveSpaces;
+    }
+
+    public void move(int moveSpaces)throws IllegalAccessException{
+        if(moveSpaces<0){
+            throw new IllegalAccessException("Move can't");
+        }
+
+        int newPosition=position+moveSpaces;
+        //最大マス数を越した場合
+        // if(newPosition>100){
+        //    position=100;
+        // }
+        position=newPosition;
+    }
+}
