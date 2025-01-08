2b52bed98984759e677114f7b19c8f0934ffdf24
Kira <kira.a0121@gmail.com>
2025-01-08 13:44:33 +0900

    Player,UserStats終了

--- backend/src/main/java/com/devbox/mavenapp/service/player/Player.java
+++ backend/src/main/java/com/devbox/mavenapp/service/player/Player.java
@@ -1,35 +1,57 @@
 package com.devbox.mavenapp.service.player;
 
 public class Player {
-    char playerID;
-    int position;
-    char turnStatus;
-    int score;
-    //List<item> item;
-    int moveSpaces;
+    public char playerID;
+    public int position;
+    public char turnStatus; // 'A' for active, 'E' for ended
+    public int score;
+    public int moveSpaces;
+    public UserStats userStats; // プレイヤーの統計情報
 
-    public Player(char playerID,int position,char turnStatus,int score,int moveSpaces){
-        this.playerID=playerID;
-        this.position=position;
-        this.turnStatus=turnStatus;
-        this.score=score;
-        this.moveSpaces=moveSpaces;
+    // コンストラクタ
+    public Player(char playerID, int position, char turnStatus, int score, int moveSpaces) {
+        this.playerID = playerID;
+        this.position = position;
+        this.turnStatus = turnStatus;
+        this.score = score;
+        this.moveSpaces = moveSpaces;
+        this.userStats = new UserStats(); // 新規インスタンスを作成
     }
 
-    public void move(int moveSpaces)throws IllegalAccessException{
-//移動マス数が－又は最大値８より大きい場合
-        if(moveSpaces<0||moveSpaces>8){
-            throw new IllegalAccessException("Move can't");
+    // ターン状態を設定
+    public void setStates(char turnStatus) {
+        this.turnStatus = turnStatus;
+    }
+
+    // プレイヤーを移動させる
+    public void move(int moveSpaces) throws IllegalAccessException {
+        // ターンが終了している場合
+        if (turnStatus == 'E') {
+            throw new IllegalAccessException("Player's turn has already ended.");
         }
 
+        // 移動マス数が範囲外の場合
+        if (moveSpaces < 0 || moveSpaces > 8) {
+            throw new IllegalAccessException("Move spaces must be between 0 and 8.");
+        }
 
-        int newPosition=position+moveSpaces;
-        //最大マス数を越した場合
-        // if(newPosition>100){
-        //    position=100;
-        // }
-        position=newPosition;
-        System.out.println(position);
+        int newPosition = position + moveSpaces;
 
+        // 最大マス数を超えた場合
+        if (newPosition > 100) {
+            position = 100; // 100に制限
+        } else {
+            position = newPosition;
+        }
+
+        System.out.println("New position: " + position);
+
+        // ターンを終了
+        turnStatus = 'E';
+    }
+
+    // ゲーム結果を更新
+    public void updateGameStats(String gameResult) {
+        userStats.updateStats(gameResult);
     }
 }
--- backend/src/main/java/com/devbox/mavenapp/service/player/UserStats.java
+++ backend/src/main/java/com/devbox/mavenapp/service/player/UserStats.java
@@ -1,25 +1,40 @@
 package com.devbox.mavenapp.service.player;
 
 public class UserStats {
-    int totalGames;
-    int firstPrize;
-    int secondPrize;
-    int thridPrize;
-    int forthPrize;
+    public int totalGames;
+    public int firstPrize;
+    public int secondPrize;
+    public int thirdPrize;
+    public int forthPrize;
 
-    public UserStats(int totalGames,
-                     int firstPrize,
-                     int secondPrize,
-                     int thridPrize,
-                     int forthPrize){
-        this.totalGames=totalGames;
-        this.firstPrize=firstPrize;
-        this.secondPrize=secondPrize;
-        this.thridPrize=thridPrize;
-        this.forthPrize=forthPrize;
+    // コンストラクタ
+    public UserStats() {
+        this.totalGames = 0;
+        this.firstPrize = 0;
+        this.secondPrize = 0;
+        this.thirdPrize = 0;
+        this.forthPrize = 0;
     }
-    public void updateStatus(int gameResult){
+
+    // ゲーム結果を元に統計を更新
+    public void updateStats(String gameResult) {
         totalGames++;
-        
+
+        switch (gameResult) {
+            case "first":
+                firstPrize++;
+                break;
+            case "second":
+                secondPrize++;
+                break;
+            case "third":
+                thirdPrize++;
+                break;
+            case "forth":
+                forthPrize++;
+                break;
+            default:
+                System.out.println("Invalid game result.");
+        }
     }
 }
